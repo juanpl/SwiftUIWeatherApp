4af3bddaeabdbebccd3efa75f497e45f834bb949
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-10-08 06:55:24 -0500

    Initial Commit

---
+++ SwiftUIWeatherApp/SwiftUIWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherAppApp.swift
+//  SwiftUIWeatherApp
+//
+//  Created by Juan Pablo Lasprilla Correa on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
